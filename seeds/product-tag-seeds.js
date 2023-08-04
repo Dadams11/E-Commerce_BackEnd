95b9750c9d675940918a464732516487e4bcc952
Dadams11 <adams.1743@osu.edu>
2023-08-03 21:38:35 -0400

    updated connection.js file

--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -1,56 +1,38 @@
-const { ProductTag } = require('../Assets/models');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-const productTagData = [
-  {
-    product_id: 1,
-    tag_id: 6,
-  },
-  {
-    product_id: 1,
-    tag_id: 7,
-  },
-  {
-    product_id: 1,
-    tag_id: 8,
-  },
-  {
-    product_id: 2,
-    tag_id: 6,
-  },
-  {
-    product_id: 3,
-    tag_id: 1,
-  },
-  {
-    product_id: 3,
-    tag_id: 3,
-  },
-  {
-    product_id: 3,
-    tag_id: 4,
-  },
-  {
-    product_id: 3,
-    tag_id: 5,
-  },
-  {
-    product_id: 4,
-    tag_id: 1,
-  },
-  {
-    product_id: 4,
-    tag_id: 2,
-  },
-  {
-    product_id: 4,
-    tag_id: 8,
-  },
-  {
-    product_id: 5,
-    tag_id: 3,
-  },
-];
+class ProductTag extends Model {}
 
-const seedProductTags = () => ProductTag.bulkCreate(productTagData);
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
 
-module.exports = seedProductTags;
+module.exports = ProductTag;
